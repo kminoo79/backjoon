eb1570ad2344300500dc913969414a2bf19c2e5b
kminoo79 <kminoo79@gmail.com>
2021-01-19 20:27:05 +0900

    2439번 문제 파이썬 코드 줄임

--- 2439.py
+++ 2439.py
@@ -1,8 +1,5 @@
 a = int(input())
 
 for x in range(1, a+1):
-    for y in range(a-x):
-        print(" ",end="")
-    for y in range(x):
-        print("*",end="")
-    print("")
+    print(" "*(a-x),end="")
+    print("*" * x)
