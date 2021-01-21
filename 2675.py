237a37f7da32b01882bce21305bfbad2ef189309
kminoo79 <kminoo79@gmail.com>
2021-01-21 20:31:19 +0900

    2675번 파이썬 +

--- 2675.py
+++ 2675.py
@@ -7,3 +7,14 @@ for x in range(n):
         for z in range(len(arr[x][0])):
             print(arr[x][1][y:y+1] * int(arr[x][0]),end="")
     print("")
+
+
+# 다른 이상적인 답 (시간 복잡도가 낮음)
+
+# t = int(input())
+# for i in range(t):
+#     num, s = input().split()
+#     text = ''
+#     for i in s:
+#         text += int(num) * i
+#     print(text)
